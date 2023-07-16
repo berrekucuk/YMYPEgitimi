10eeb4efa85fb4960859bc752e77cecbab0008ea
berre küçük <beRrekucuk@hotmail.com>
2023-07-16 14:29:55 +0300

    .

---
+++ 05.Listeler/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.WriteLine("Hello, World!");
+
+
+// [] => listeleri temsil eder => array işareti
+// string[] ames ={"asdas", "asdasd"};
+//index numarası desem => 0'dan başlar
+//string[] names = new string [3];
+//names[0] = "Taner";
+//names[1] = "Taner";
+//names[2] = "Taner";
+
+//list<string> names = new List<string>(){"asdas" , "asdasd"};
+
+List<string> names = new(); //Örneğe dönüştürmek için new kelimesini kullanıyoruz. Örneğe dönüştürme işleminde yazılımda "instance"üretme denir.
+//List<string> yapısında sayı belirtmemize gerek yok
+
+names.Add("Taner");
+names.Add("Taner");
+names.Add("Taner")
+names.Add("Taner");
+names.Add("Taner");
+names.Add("Taner");
+
+//class User
+//{
+// public string Name;
+//}
+
