9593b2a77f909146cdc09614d61575ef51d88c7e
berre küçük <beRrekucuk@hotmail.com>
2023-07-16 12:41:36 +0300

    .

--- 01.Degiskenler/Program.cs
+++ 01.Degiskenler/Program.cs
@@ -1,3 +1,31 @@
 ﻿
 Console.WriteLine("Hello, World!");
+
+//string,int,decimal,double,float,bool,DateTime,Object
+//float,double: istisni para birimlerinde kullanılır.
+
+string name; //null 
+int age; //0
+bool isActive; //false
+
+
+float n1 = 100;
+int n2 = 200;
+var n3 = n1 + n2;
+Console.WriteLine(n3.GetType());
+
+string nameLastName = "Berre Küçük";
+
+name = "asdasd";
+char karakter = 'A';
+//string kodunda kelime olarak yazarken char kodunda sadece harf kodlanabilir.
+string aciklama = "asddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd";
+
+Console.WriteLine(nameLastName);
+
+
+
+
+
+
 Console.ReadLine();  //Programın çalışmaya devam etmesini istersek bu kodu kullanabiliriz.
